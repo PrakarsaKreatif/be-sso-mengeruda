2473beddf52ec52564b0f305374afc7f2d818fc7
Thobiq <malyasyithobiq@gmail.com>
2026-09-20 22:13:14 +0700

    add allowed origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,30 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173', 
+        'http://localhost:5174', 
+        'http://localhost:5175', 
+        'http://localhost:5176', 
+        'http://localhost:3000',
+        'http://localhost:3002',
+        'http://localhost:3003',
+        'http://127.0.0.1:5173',
+        'http://127.0.0.1:5174',
+        'http://127.0.0.1:5175',
+        'http://127.0.0.1:5176',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:3002',
+        'http://127.0.0.1:3003',
+        'https://sso.mengeruda.id',
+        'https://mengeruda.id',
+        'https://www.mengeruda.id',
+        'http://mengeruda.id',
+        'http://www.mengeruda.id',
+        'https://tourism.mengeruda.id',
+        'https://e-surat.mengeruda.id',
+        'http://e-surat.mengeruda.id',
+    ],
 
     'allowed_origins_patterns' => [
         '/.*\.mengeruda\.id$/',
